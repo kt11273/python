603733151f56171fc3f9306f1cbdb4301bf02475
kt11273 <kt11273@gmail.com>
2016-01-03 00:30:19 +0800

    example of crawler

---
+++ ex_crawler.py
@@ -0,0 +1,18 @@
+'''
+#File Name:	ex_crawler.py
+#File Disc:	how to crawler one web address
+#Other:		try & except
+#Date:		2013/01/xx
+'''
+import urllib2
+import re
+import io
+import sys
+
+web_type = sys.getfilesystemencoding()
+url = 'http://book.sfacg.com/Novel/31979/61619/410355/'
+req = urllib2.Request(url)
+html_src = urllib2.urlopen(req).read().decode('utf-8').encode(web_type)
+
+title = re.findall('<div class="list_menu_title".+>(.+)</div>', html_src)[0]
+print title
